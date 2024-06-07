72c05084dc321bc51175ac99a67df7097791f2fe
Ahmed Khan <ahmed.khan@smallcase.com>
2024-06-07 13:13:52 +0000

    main

--- main.go
+++ main.go
@@ -2,21 +2,6 @@ package main
 
 import "fmt"
 
-// TC - O(N), SC - O(N)
-func containsDuplicate(A []int) bool {
-	checkMap := map[int]int{}
-
-	for i := range A {
-		if _, ok := checkMap[A[i]]; ok {
-			return true
-		} else {
-			checkMap[A[i]] = 1
-		}
-	}
-	return false
-}
-
 func main() {
-	arr := []int{1, 2, 3, 1}
-	fmt.Println(containsDuplicate(arr))
+	fmt.Println("main")
 }
