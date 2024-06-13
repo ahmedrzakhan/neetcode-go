0cfd10427a23660b511ddced28912b60c6a7f0ad
Ahmed Khan <ahmed.khan@smallcase.com>
2024-06-13 01:28:00 +0000

    rll

--- linkedList/reverseLinkedList.go
+++ linkedList/reverseLinkedList.go
@@ -5,7 +5,7 @@ import "fmt"
 // https://leetcode.com/problems/reverse-linked-list/
 
 type ListNode struct {
-	head *ListNode
+	Val  int
 	Next *ListNode
 }
 
@@ -28,6 +28,26 @@ func reverseList(head *ListNode) *ListNode {
 	return prev
 }
 
+func printList(head *ListNode) {
+	for head != nil {
+		fmt.Print(head.Val, " ")
+		head = head.Next
+	}
+	fmt.Println()
+}
+
 func mainRLL() {
-	fmt.Println("main")
+	// Example: 1 -> 2 -> 3 -> 4 -> 5 becomes 5 -> 4 -> 3 -> 2 -> 1
+	head := &ListNode{Val: 1}
+	head.Next = &ListNode{Val: 2}
+	head.Next.Next = &ListNode{Val: 3}
+	head.Next.Next.Next = &ListNode{Val: 4}
+	head.Next.Next.Next.Next = &ListNode{Val: 5}
+	fmt.Println("Original List:")
+	printList(head)
+
+	head = reverseList(head)
+
+	fmt.Println("Reversed List:")
+	printList(head)
 }
